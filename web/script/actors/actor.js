58cc9295c976e52798b38b1c85fd48c21a647222
unknown <plaguedpixel@gmail.com>
2020-05-17 11:40:56 +0800

    Added in test for null role colour

--- web/script/actors/actor.js
+++ web/script/actors/actor.js
@@ -129,7 +129,7 @@ Actor.prototype.updateSprite = function() {
     }
     if(this.talking) this.messageBox.updateScreen();
     this.sprite.metrics = metrics;
-    console.log('Role Colour:', this.roleColor);
+    if (this.roleColor) { console.log('Role Colour:', this.roleColor);} else {console.log('Null role colour');}
     this.sprite.image = this.roleColor ? [this.roleColor,'actors'] : 'actors';
 };
 
